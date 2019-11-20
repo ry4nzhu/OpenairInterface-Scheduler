766812ab7b67ad3876b9cae2fd1fa96fb81287a3
ry4nzhu <ryanzhu@umich.edu>
2019-11-19 21:14:18 -0500

    add cli & srv shm

---
+++ openair2/LAYER2/MAC/UE-app-notify-srv.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define IP_PROTOCOL 0
+#define TXRXFLAG 0
+#define BIND_PORT 31671
+#define BIND_IP "10.0.1.2"
+#define BUF_SIZE 1024
+#define REQUEST_STR "GET_APP_TYPE"
+#define RESPONSE_STR "12"
+
+
+void clearBuf(char* b) {
+	memset(b, 0, BUF_SIZE);
+}
+
+
+int appTypeBuf(char* b) {
+    strncpy(b, RESPONSE_STR, BUF_SIZE);
+	return strlen(b);
+}
+
+
+int main(int argc, char *argv[]) {
+    int sockfd, cfd, nBytes, sentBytes, len, rc;
+    struct sockaddr_in my_addr, peer_addr;
+    socklen_t my_addr_size, peer_addr_size;
+    char buf[BUF_SIZE];
+
+
+    sockfd = socket(AF_INET, SOCK_STREAM, IP_PROTOCOL);
+    if (sockfd < 0) {
+        perror("socket:");
+		return 1;
+	}
+
+    my_addr_size = sizeof(struct sockaddr_in);
+    memset(&my_addr, 0, my_addr_size);
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(BIND_PORT);
+    my_addr.sin_addr.s_addr = inet_addr(BIND_IP);
+
+    if (bind(sockfd, (struct sockaddr *) &my_addr, my_addr_size) == -1) {
+        perror("bind:");
+		return 1;
+	}
+
+    if (listen(sockfd, 1) == -1) {
+        perror("listen:");
+		return 1;
+	}
+
+    while (1) {
+        printf("\nWaiting for a TCP request...\n");
+
+		// accept a TCP peer connection
+        peer_addr_size = sizeof(struct sockaddr_in);
+        memset(&peer_addr, 0, peer_addr_size);
+        cfd = accept(sockfd, (struct sockaddr *) &peer_addr, &peer_addr_size);
+        if (cfd == -1) {
+            perror("accept:");
+			continue;
+		}
+
+        printf("\nConnected to a client...\n");
+
+		
+        // receive & send
+		while (1) {
+			printf("\nWaiting to receive data\n");
+			clearBuf(buf);
+			nBytes = recv(cfd, buf, BUF_SIZE, TXRXFLAG);
+			if (nBytes == -1) {
+				perror("recv:");
+				break;
+			}
+			else if (nBytes == 0) {
+				printf("\n0 bytes are read from the socket...\n");
+				break;
+			}
+			else {
+				// valid request
+				if (strncmp(buf, REQUEST_STR, nBytes) == 0) {
+					clearBuf(buf);
+					len = appTypeBuf(buf);
+					sentBytes = 0;
+					while (sentBytes < len) {
+						rc = send(cfd, buf+sentBytes, len, TXRXFLAG);
+						if (rc == -1) {
+							perror("sent:");
+							break;
+						}
+						sentBytes += rc;
+					}
+					if (sentBytes == len) {
+						printf("\nApplication type is successfully sent to peer...\n");
+						continue;
+					}
+					else {
+						printf("\nProblem on sending!\n");
+						break;
+					}
+				}
+				// invalid request
+				else {
+					printf("\nInvalid request is received!\n");
+					break;
+				}
+			}
+		}
+
+		close(cfd);
+	}
+
+	return 0;
+}
+
+
---
+++ openair2/LAYER2/MAC/eNB-app-notify-cli.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <limits.h>
+
+#define IP_PROTOCOL 0
+#define IP_ADDRESS "10.0.1.2"
+#define PORT_NO 31671
+#define BUF_SIZE 1024
+#define sendrecvflag 0
+#define REQUEST_STR "GET_APP_TYPE"
+#define SHM_BUF_SIZE 1024
+#define SHM_KEY 0x1234
+
+
+struct shmseg {
+	int ue_app_type;
+};
+
+int _UE_app_type = -1;
+
+int req2buf(char *buf, int size) {
+	strncpy(buf, REQUEST_STR, size);
+	return strlen(buf);
+}
+
+int main(int argc, char *argv[]) {
+    char buf[BUF_SIZE] = {0};
+	int len;
+	/* socket programming */
+    int sockfd, nBytes;
+    struct sockaddr_in addr_con;
+    int addrlen = sizeof(addr_con);
+    addr_con.sin_family = AF_INET;
+    addr_con.sin_port = htons(PORT_NO);
+    addr_con.sin_addr.s_addr = inet_addr(IP_ADDRESS);
+	/* shared memory */
+	int shmid;
+   	struct shmseg *shmp;
+	/* strtol */
+	int base = 10;
+	char *endptr = NULL;
+	long val;
+
+	sockfd = socket(AF_INET, SOCK_STREAM, IP_PROTOCOL);
+	if (sockfd < 0) {
+		perror("socket:");
+		return 1;
+	}
+
+	if (-1 == connect(sockfd, (struct sockaddr *) &addr_con, addrlen)) {
+		perror("connect:");
+		return 1;
+	}
+	printf("Connected to the server.\n");
+
+   	shmid = shmget(SHM_KEY, sizeof(struct shmseg), 0644|IPC_CREAT);
+   	if (shmid == -1) {
+   	   perror("Shared memory");
+   	   return 1;
+   	}
+   	
+   	// Attach to the segment to get a pointer to it.
+   	shmp = shmat(shmid, NULL, 0);
+   	if (shmp == (void *) -1) {
+   	   perror("Shared memory attach");
+   	   return 1;
+   	}
+
+	while (1) {
+		// send
+		len = req2buf(buf, sizeof(buf));
+		send(sockfd, buf, len, sendrecvflag);
+		// TODO: handle error
+
+		// receive
+		memset(buf, 0, sizeof(buf));
+		nBytes = recv(sockfd, buf, sizeof(buf), sendrecvflag);
+		// TODO: handle error
+
+		// process
+		if (nBytes > 0) {
+			printf("Received: %s\n", buf);
+
+			errno = 0;    /* To distinguish success/failure after call */
+			val = strtol(buf, &endptr, base);
+			/* Check for various possible errors */
+			if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
+				   || (errno != 0 && val == 0)) {
+				perror("strtol");
+				exit(EXIT_FAILURE);
+			}
+
+			if (endptr == buf) {
+				fprintf(stderr, "No digits were found\n");
+				exit(EXIT_FAILURE);
+			}
+			/* If we got here, strtol() successfully parsed a number */
+			_UE_app_type = (int) val;
+			
+			/* Transfer blocks of data from buffer to shared memory */
+			// TODO: mutex
+			shmp->ue_app_type = _UE_app_type;
+
+			sleep(1);
+		}
+	}
+
+	close(sockfd);
+
+   	if (shmdt(shmp) == -1) {
+   	   perror("shmdt");
+   	   return 1;
+   	}
+
+   	if (shmctl(shmid, IPC_RMID, 0) == -1) {
+   	   perror("shmctl");
+   	   return 1;
+   	}
+
+	return 0;
+}
+
+
